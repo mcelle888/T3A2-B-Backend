93a124a2c7c53bb9b3283b5cc4ebb9b899c09e8b
mcelle888 <14894@coderacademy.edu.au>
2024-02-17 17:25:53 +1100

    Setup Mongo DB

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 const rsvp = ["name", "number", "email"]
 
@@ -6,6 +7,24 @@ const reply = [
     {name: 'Michelle', number: '0400000230', email: 'michelle@gmail.com'},
     {name: 'Tim', number: '0434000200', email: 'tim@gmail.com'}
 ]
+
+mongoose.connect("")
+    .then(m => console.log(m.connection.readyState === 1 ? 'MongoDB connected!' : 'MongoDB failed  to connect'))
+    .catch(err => console.log(err))
+
+const repliesSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    number: { type: String, required: true },
+    email: { type: String, required: true },
+    ceremony: { type: String, required: true },
+    reception: { type: String, required: true },
+    guests: String,
+    dietry: String,
+    message: String,
+
+})
+
+const ReplyModel = mongoose.model('Reply', repliesSchema)
 
 const app = express()
 
@@ -26,15 +45,18 @@ app.get('/responses/:id', (req, res) => {
     }
 })
 
-
 // Post request
 
-app.post('/rsvp',(req, res) => {
-    console.log(req.body)
+app.post('/rsvp', async (req, res) => {
+    try {
 
-    reply.push(req.body)
+    const insertedReply = await ReplyModel.create(req.body)
+    res.status(201).send(insertedReply)
+    } 
+    catch (err) {
+        res.status(400).send( {error: err.message} )
+    }
 
-    res.status(201).send(reply[reply.length -1])
 })
 
 app.listen(8001)
