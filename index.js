0d8bef45be0def99a91aec55791c708f4682c01b
mcelle888 <14894@coderacademy.edu.au>
2024-02-22 00:20:35 +1100

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import { DetailModel } from './db.js'
 import rsvpRoutes from './routes/rsvp_routes.js'
+import cors from 'cors'
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
